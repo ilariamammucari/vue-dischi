c6a0314c4d606fcef6920bdc520e8070d56f9c1d
Ilaria Mammucari <ilariamammucari@hotmail.it>
2021-02-09 14:25:55 +0100

    dischi musicali

--- vue/script.js
+++ vue/script.js
@@ -1,3 +1,27 @@
 //! API: https://flynn.boolean.careers/exercises/api/array/music
 
-alert();
+var app = new Vue({
+    el: '#root',
+    data: {
+        dischi: '',
+        generi: [],
+        valoreGenere: ''
+    },
+    mounted(){
+        axios.get('https://flynn.boolean.careers/exercises/api/array/music').then((result) => {
+
+            this.dischi = result.data.response;
+
+            this.filtroGeneri();
+        });
+    },
+    methods: {
+        filtroGeneri(){
+            this.dischi.forEach(element => {
+                if ( !this.generi.includes(element.genre) ){
+                    this.generi.push(element.genre);
+                }
+            });
+        }
+    }
+});
